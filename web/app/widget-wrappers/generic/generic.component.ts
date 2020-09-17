a162f59e6265b0f135a4b9468e5ced3768323a90
Wagner O. Wutzke <wagner.wutzke@nordeck.net>
2020-09-17 10:01:38 -0300

    > Added url params: user_id, room_id, session_token (Base64 encoded)

--- web/app/widget-wrappers/generic/generic.component.ts
+++ web/app/widget-wrappers/generic/generic.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 import { WidgetApiService } from "../../shared/services/integrations/widget-api.service";
+import { SessionStorage } from "../../shared/SessionStorage";
 
 @Component({
     selector: "my-generic-widget-wrapper",
@@ -17,10 +18,16 @@ export class GenericWidgetWrapperComponent {
     constructor(widgetApi: WidgetApiService, activatedRoute: ActivatedRoute, sanitizer: DomSanitizer) {
         let params: any = activatedRoute.snapshot.queryParams;
 
+        const user_id = encodeURI(SessionStorage.userId);
+        const room_id = encodeURI(SessionStorage.roomId);
+        const session_token = localStorage.getItem("mx_access_token");
+        const token_B64 = btoa(session_token);
+        const url_params = "&user_id=" + user_id + "&room_id=" + room_id + "&session_token=" + token_B64;
+
         widgetApi.isEmbeddable(params.url).then(result => {
             this.canEmbed = result.canEmbed;
             this.isLoading = false;
-            this.embedUrl = sanitizer.bypassSecurityTrustResourceUrl(params.url);
+            this.embedUrl = sanitizer.bypassSecurityTrustResourceUrl(params.url) + url_params;
         }).catch(err => {
             console.error(err);
             this.canEmbed = false;
