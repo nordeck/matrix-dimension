3b184d0c05f680d6d13f2b307c279701e18944e9
nurjinn jafar <nurjin.jafar@nordeck.net>
2020-10-29 12:18:02 +0100

    fixed class name

--- src/api/dimension/dimensionUserService.ts
+++ src/api/dimension/dimensionUserService.ts
@@ -13,7 +13,7 @@ interface DimensionUserConfigResponse {
  * Administrative API for general information about Dimension
  */
 @Path("/api/v1/dimension")
-export class AdminService {
+export class DimensionUserService {
 
     @GET
     @Path("config")
